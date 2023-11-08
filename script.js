6e6b34140c120d2d3f2ba79aa972aeab4fdb3d30
vijay00900 <150218484+vijay00900@users.noreply.github.com>
2023-11-08 11:42:20 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
-.container {
-    text-align: center;
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const paddleWidth = 10;
+const paddleHeight = 100;
+let ballX = canvas.width / 2;
+let ballY = canvas.height / 2;
+let ballSpeedX = 5;
+let ballSpeedY = 5;
+let paddleY = canvas.height / 2 - paddleHeight / 2;
+let playerScore = 0;
+
+function updateGameArea() {
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Update game state
+    // Handle user input for paddle movement
+
+    // Move the ball
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
+
+    // Check for collisions with walls, paddles, and update score
+
+    // Render the game elements
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, paddleY, paddleWidth, paddleHeight);
+    ctx.fillRect(ballX, ballY, 10, 10);
+
+    // Display the score
+    ctx.fillText("Score: " + playerScore, 10, 20);
 }
 
-canvas {
-    background: black;
+// Game loop
+function gameLoop() {
+    updateGameArea();
+    requestAnimationFrame(gameLoop);
 }
 
-#score {
-    color: white;
-    font-size: 24px;
-}
+// Start the game loop
+gameLoop();
